3998bdca47cdff44c18f84eb6e01b54c2aa59124
Evin Tunador <evintunador@Evins-Air.localdomain>
2025-02-21 14:14:21 -0500

    dropout

--- 04_dropout/dropout.py
+++ 04_dropout/dropout.py
@@ -1,6 +1,14 @@
+"""
+This tutorial on low-memory dropout required the least editing of all the original Triton documentation tutorials
+
+What you'll learn:
+- Parallel pseudo-random number generation
+- Parallelizing across batch dimension
+"""
 import torch
 import triton
 import triton.language as tl
+import math
 
 DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
 
@@ -9,14 +17,14 @@ def _seeded_dropout(
     x_ptr,
     output_ptr,
     n_elements,
-    p,
-    seed,
+    B,
+    p, # a float32 probability, so range [0,1]
+    seed, # a single int32
     BLOCK_SIZE: tl.constexpr,
 ):
-    # compute memory offsets of elements handled by this instance
+    # compute memory offsets of elements handled by this program
     pid = tl.program_id(axis=0)
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    offsets = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     # load data from x
     mask = offsets < n_elements
     x = tl.load(x_ptr + offsets, mask=mask)
@@ -32,23 +40,13 @@ def seeded_dropout(x, p, seed):
     output = torch.empty_like(x)
     assert x.is_contiguous()
     n_elements = x.numel()
+    B = math.prod(x.shape[:-1])
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
     _seeded_dropout[grid](x, output, n_elements, p, seed, BLOCK_SIZE=1024)
     return output
 
-x = torch.randn(size=(10, ), device=DEVICE)
-# Compare this to the baseline - dropout mask is never instantiated!
-output = seeded_dropout(x, p=0.5, seed=123)
+x = torch.randn(size=(4, 10), device=DEVICE)
+output1 = seeded_dropout(x, p=0.5, seed=123)
 output2 = seeded_dropout(x, p=0.5, seed=123)
-output3 = seeded_dropout(x, p=0.5, seed=512)
-print(x, output, output2, output3)
-
-"""
-Exercises
-1. Extend the kernel to operate over a matrix and use a vector of seeds -
-one per row.
-2. Add support for striding.
-3. (challenge) Implement a kernel for sparse Johnson-Lindenstrauss
-transform which generates the projec"on matrix on the fly each "me
-using a seed.
-'''
+output3 = seeded_dropout(x, p=0.5, seed=321)
+print(x, output1, output2, output3)
