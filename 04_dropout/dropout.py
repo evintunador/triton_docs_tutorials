3bb8763e71ab878a9c99e2bc6eb5f8b573a7bbe2
Evin Tunador <evintunador@Evins-Air.localdomain>
2025-02-21 14:15:01 -0500

    oops forgot to save dropout

--- 04_dropout/dropout.py
+++ 04_dropout/dropout.py
@@ -3,12 +3,10 @@ This tutorial on low-memory dropout required the least editing of all the origin
 
 What you'll learn:
 - Parallel pseudo-random number generation
-- Parallelizing across batch dimension
 """
 import torch
 import triton
 import triton.language as tl
-import math
 
 DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
 
@@ -17,7 +15,6 @@ def _seeded_dropout(
     x_ptr,
     output_ptr,
     n_elements,
-    B,
     p, # a float32 probability, so range [0,1]
     seed, # a single int32
     BLOCK_SIZE: tl.constexpr,
@@ -27,26 +24,27 @@ def _seeded_dropout(
     offsets = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     # load data from x
     mask = offsets < n_elements
-    x = tl.load(x_ptr + offsets, mask=mask)
-    # generates a block of uniformly distributed float32 values in [0, 1), given a seed and a block of int32 oﬀsets
-    random = tl.rand(seed, offsets)
-    # randomly prune it
+    x = tl.load(x_ptr + offsets, mask=mask) # shape (BLOCK_SIZE)
+    # the key insight is that we generate and use a mask entirely in SRAM without ever having to store it in DRAM.
+    # this line generates uniformly distributed float32 values in [0, 1), given a seed and a block of int32 oﬀsets
+    random = tl.rand(seed, offsets) # shape (BLOCK_SIZE)
+    # prune based on our desired probability threshold
     x_keep = random > p # true or false so full of 0's and 1's
-    # write-back
     output = tl.where(x_keep, x / (1 - p), 0.0)
+        # where x_keep is True, the value is x/(1-p), and where False it's 0.0
+    # write-back to DRAM
     tl.store(output_ptr + offsets, output, mask=mask)
 
 def seeded_dropout(x, p, seed):
     output = torch.empty_like(x)
     assert x.is_contiguous()
     n_elements = x.numel()
-    B = math.prod(x.shape[:-1])
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
     _seeded_dropout[grid](x, output, n_elements, p, seed, BLOCK_SIZE=1024)
     return output
 
-x = torch.randn(size=(4, 10), device=DEVICE)
+x = torch.randn(size=(10, ), device=DEVICE)
 output1 = seeded_dropout(x, p=0.5, seed=123)
 output2 = seeded_dropout(x, p=0.5, seed=123)
-output3 = seeded_dropout(x, p=0.5, seed=321)
+output3 = seeded_dropout(x, p=0.5, seed=512)
 print(x, output1, output2, output3)
